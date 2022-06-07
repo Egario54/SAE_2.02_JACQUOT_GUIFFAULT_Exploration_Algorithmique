9ca66c7418465034d34cac67fefac1c2cbe333d0
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2022-06-07 14:46:21 +0200

    Question 1 & 2

--- Noeud.java
+++ Noeud.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Noeud {
+
+    private String nom;
+
+    private List<Arc> adj;
+    public Noeud (String n){
+        this.nom = n;
+        this.adj = new ArrayList<Arc>();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        Noeud n = (Noeud) obj;
+        return ((n.getNom() == nom));
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
 }
