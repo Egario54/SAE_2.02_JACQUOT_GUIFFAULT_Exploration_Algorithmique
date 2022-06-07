e3729afca9d53d90d4fe5113ac57cead97659138
Egario54 <thierry.jacquot2@etu.univ-lorraine.fr>
2022-06-07 15:08:58 +0200

    Ajout de la Javadoc de Noeud

--- java_etudiant/src/Noeud.java
+++ java_etudiant/src/Noeud.java
@@ -3,26 +3,48 @@ import java.util.List;
 
 public class Noeud {
 
+    /*
+        Nom du noeud
+         */
     private String nom;
 
+    /*
+    Liste des arcs qui partent du noeud
+     */
     private List<Arc> adj;
+
+    /*
+    Crée un noeud nomme avec une liste vide d'arcs
+    @Param String nom du noeud
+     */
     public Noeud (String n){
         this.nom = n;
         this.adj = new ArrayList<Arc>();
     }
 
+    /*
+    Methode qui compare le nom de deux noeuds
+    @param Object noeud
+    @return boolean
+     */
     @Override
     public boolean equals(Object obj){
         Noeud n = (Noeud) obj;
         return ((n.getNom() == nom));
     }
 
-    public String getNom(){
-        return this.nom;
-    }
-
+    /*
+    Methode qui crée un arc et qui l'ajoute a la liste
+    @param String destination de l'arc
+    @param double cout de l'arc
+     */
     public void ajouterArc(String destination, double cout){
         Arc arc = new Arc (destination,cout);
         adj.add(arc);
     }
+
+    public String getNom(){
+        return this.nom;
+    }
+
 }
